23c25db08901ea816a089f124db8d970843ff147
unstoppableking <unstoppable0_0@sina.com>
2019-10-06 21:25:04 +0800

    单线程版本

--- main_v2.py
+++ main_v2.py
@@ -7,6 +7,7 @@ Created on Sat Oct  5 09:50:38 2019
 """
 import os
 from docx import Document
+from docx.oxml.ns import qn
 from Translator import Translator
 
 
@@ -21,14 +22,18 @@ def read_file(file_name):
 # 相比前版本改动此处函数,但兼容上一版本的此函数
 def save_file(content, dict_content=None):
     document = Document()
+    document.styles['Normal'].font.name = u'宋体'
+    document.styles['Normal']._element.rPr.rFonts.set(qn('w:eastAsia'), u'宋体')
+    document.styles['Body Text'].font.name = u'Time New Roman'
+    document.styles['Body Text']._element.rPr.rFonts.set(qn('w:english'), u'Time New Roman')
     hot = True
     if dict_content==None:
         hot = False
     for p_n in range(len(content)):
         if hot:
-            document.add_paragraph(dict_content[p_n])
+            document.add_paragraph(dict_content[p_n], style='Body Text')
         document.add_paragraph(content[p_n])
-    document.save(content[0][0:5]+'.docx') #保存文档
+    document.save(content[0][0:5]+'.docx')  # 保存文档
 
 
 def translate(dict_content):
