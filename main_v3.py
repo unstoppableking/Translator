64fdb06b60dcc4e67f255a9492d071a1a15ef79f
unstoppableking <unstoppable0_0@sina.com>
2019-10-06 19:36:24 +0800

    多线程版本，效果不是太明显

--- main_v3.py
+++ main_v3.py
@@ -3,8 +3,9 @@
 Created on Sat Oct  5 09:50:38 2019
 更新概况:
 1.读取docx文件，将翻译完的内容和英文内容一起保存在新文件中;
-2.开启多线程;
-3.兼容前面所有版本
+2.开启多线程,但效果不是很明显;
+3.不兼容前面版本
+4.增加显示进度
 @author: XL
 """
 import os
@@ -19,6 +20,7 @@ def read_file(file_name):
         file_content.append(con.text)
     return file_content
 
+
 # 此处为v2版本改动函数,但兼容上一版本的此函数
 def save_file(content, dict_content=None):
     document = Document()
@@ -29,39 +31,49 @@ def save_file(content, dict_content=None):
         if hot:
             document.add_paragraph(dict_content[p_n])
         document.add_paragraph(content[p_n])
-    document.save(content[0][0:5]+'.docx') #保存文档
+    document.save(content[0][0:5]+'.docx')  # 保存文档
 
-def translate(dict_content):
+
+# 此处为v3版本改动函数,不兼容上一版本的此函数
+def translate(dict_content, translator, num):
     content = {}
-    translator = Translator()
-    for index,con in dict_content.items():
+    for index, con in dict_content.items():
         if len(con) < 7:
             content[index] = con
         else:
             con1 = translator.translate(con)
-#            print('size = ', len(con1))
+            print('Translate para %s/%s' % (index, num))
             content[index] = con1
     return content
 
 
-# 此处为v3版本改动函数,但兼容上一版本的此函数
-def main(mulp = False):
+# 此处为v3版本改动,不兼容上一版本函数
+def main(mulp=False):
     path = r'C:\Users\XL\Documents\GitHub\GoogleTranslator'.replace('\\', '/')
     file_list = ['t_3. Manuscript.docx', 't_4. Manuscript.docx']
+    translator = Translator()
     for f in file_list:
         file = os.path.join(path, f)
         file_content = read_file(file)
         index = range(0, len(file_content))
         dict_content = dict(zip(index, file_content))
+        # fun_temp = lambda dict_c: translate(dict_c, translator)
         if mulp:
             content = {}
-            pool = Pool(4)
-            content.update(pool.map(translate, dict_content))
+            pool = Pool(10)
+            for i, j in dict_content.items():
+                # a = pool.apply_async(translate, ({i: j}, translator)).get()
+                content.update(pool.apply_async(translate, ({i: j}, translator, len(dict_content))).get())
+            # temp = pool.map(fun_temp, dict_content)
+            # for di in temp:
+            #     content.update(di)
+            # content.update(pool.map(fun_temp, dict_content))
             pool.close()
             pool.join()
         else:
-            content = translate(dict_content)
+            content = translate(dict_content, translator)
         save_file(content, dict_content)
 
+
 if __name__ == '__main__':
-    main(mulp = True)
+    main(mulp=True)
