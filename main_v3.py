f33c7723bf814341c80e2b738b2f7c77ed48f2a9
unstoppableking <unstoppable0_0@sina.com>
2019-10-06 17:32:10 +0800

    版本更新

--- main_v3.py
+++ main_v3.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Oct  5 09:50:38 2019
-读取docx文件，将翻译完的内容和英文内容一起保存在新文件中
+更新概况:
+1.读取docx文件，将翻译完的内容和英文内容一起保存在新文件中;
+2.开启多线程;
+3.兼容前面所有版本
 @author: XL
 """
 import os
@@ -16,7 +19,7 @@ def read_file(file_name):
         file_content.append(con.text)
     return file_content
 
-# 相比前版本改动此处函数,但兼容上一版本的此函数
+# 此处为v2版本改动函数,但兼容上一版本的此函数
 def save_file(content, dict_content=None):
     document = Document()
     hot = True
@@ -41,7 +44,8 @@ def translate(dict_content):
     return content
 
 
-def main():
+# 此处为v3版本改动函数,但兼容上一版本的此函数
+def main(mulp = False):
     path = r'C:\Users\XL\Documents\GitHub\GoogleTranslator'.replace('\\', '/')
     file_list = ['t_3. Manuscript.docx', 't_4. Manuscript.docx']
     for f in file_list:
@@ -49,12 +53,15 @@ def main():
         file_content = read_file(file)
         index = range(0, len(file_content))
         dict_content = dict(zip(index, file_content))
-#        pool = Pool(4)
-#        pool.map(translate, dict_content)
-#        pool.close()
-#        pool.join()
-        content = translate(dict_content)
+        if mulp:
+            content = {}
+            pool = Pool(4)
+            content.update(pool.map(translate, dict_content))
+            pool.close()
+            pool.join()
+        else:
+            content = translate(dict_content)
         save_file(content, dict_content)
 
 if __name__ == '__main__':
-    main()
+    main(mulp = True)
